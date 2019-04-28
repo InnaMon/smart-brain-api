d88d91e24651f2c73b70531e72fda672de52e81c
Inna Mon <inna.mon@gmail.com>
2019-04-28 16:52:56 -0500

    added get method for user to login in with id

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const database = {
             id: '123',
             name: 'John',
             email: 'john@gmail.com',
-            password: 'cookies',
             entries: 0,
             joined: new Date()
         },
@@ -19,9 +18,15 @@ const database = {
             id: '124',
             name: 'Sally',
             email: 'sally@gmail.com',
-            password: 'bananas',
             entries: 0,
             joined: new Date()
+        }
+    ],
+    login: [
+        {
+            id: '987',
+            hash: '',
+            email: 'john@gmail.com'
         }
     ]
 }
@@ -50,6 +55,20 @@ app.post('/register', (req, res) => {
         joined: new Date()
     })
     res.json(database.users[database.users.length-1]);
+})
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+    })
+    if (!found) {
+        res.status(400).json('not found');
+    }
 })
 
 app.listen(3000, () => {
