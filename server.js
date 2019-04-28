0b70e8096a88447d120288a2cc2f2c24cba6e793
Inna Mon <inna.mon@gmail.com>
2019-04-27 20:41:46 -0500

    added post method that compares users input method with their stored database info

--- server.js
+++ server.js
@@ -28,7 +28,12 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signin', (req, res) => {
-    res.json("signing")
+    if (req.body.email === database.users[0].email &&
+    req.body.password === database.users[0].password) {
+        res.json('success!');
+    } else {
+        res.status(400).json('error logging in');
+    }
 });
 
 app.listen(3000, () => {
