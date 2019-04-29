15ad646ca707d7644cdaeffc012a668b4df7a766
Inna Mon <inna.mon@gmail.com>
2019-04-29 11:07:06 -0500

    removed password property from request object in register method to optimize security. Added edits to file to ensure server works properly

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.post('/signin', (req, res) => {
     });
     if (req.body.email === database.users[0].email &&
     req.body.password === database.users[0].password) {
-        res.json('success!');
+        res.json(database.users[0]);
     } else {
         res.status(400).json('error logging in');
     }
@@ -64,7 +64,7 @@ app.post('/register', (req, res) => {
         id: '125',
         name: name,
         email: email,
-        password: password,
+        //password: password, DO NOT WANT PASSWORD RETURNED 
         entries: 0,
         joined: new Date()
     })
