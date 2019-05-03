46af6c787c9d5aae0953113af52a7be3df3c4a84
Inna Mon <inna.mon@gmail.com>
2019-05-03 09:18:08 -0500

    added knex postgres object to register method in order to send user input info directly to data base

--- server.js
+++ server.js
@@ -2,14 +2,20 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
-const knex = require('knex')({
+const knex = require('knex');
+
+const postgres = knex({
     client: 'pg',
     connection: {
       host : '127.0.0.1',
       user : 'postgres',
-      password : '',
+      password : 'monjoseph7',
       database : 'smart-brain'
     }
+  });
+
+  postgres.select('*').from('users').then(data => {
+      console.log(data);
   });
 
 const app = express();
@@ -66,17 +72,11 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
-    bcrypt.hash(password, null, null, function(err, hash) {
-        console.log(hash);
-    });
-    database.users.push({
-        id: '125',
-        name: name,
+    postgres('users').insert({
         email: email,
-        //password: password, DO NOT WANT PASSWORD RETURNED 
-        entries: 0,
+        name: name,
         joined: new Date()
-    })
+    }).then(console.log);
     res.json(database.users[database.users.length-1]);
 })
 
