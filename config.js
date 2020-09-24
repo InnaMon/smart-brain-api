620a385c8dbaa951c66de8050abc4b7573308179
Inna Mon <inna.mon@gmail.com>
2020-09-24 17:01:22 -0500

    removed database_url from .env file and instead pulled into heroku global variable directly through global variables

--- config.js
+++ config.js
@@ -1,7 +1,5 @@
 const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
-    PORT: process.env.PORT,
-    CLARIFAI_API: process.env.CLARIFAI_API,
-    DATABASE_URL: process.env.DATABASE_URL
+    CLARIFAI_API: process.env.CLARIFAI_API
 };
